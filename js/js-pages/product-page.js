6d20b19b86603bd58e50ac3a5247b522977617ad
RobertDacian <tennerdigitalart@gmail.com>
2022-04-23 11:43:36 -0300

    update

--- js/js-pages/product-page.js
+++ js/js-pages/product-page.js
@@ -9,76 +9,137 @@ import { message } from "../components/messages.js";
 //////////////////////////////////////////////////////////////////////////////
 
 const productsContainer = document.querySelector(".products-container");
+const perPage = document.querySelector(".per-page-selection");
+const categories = document.querySelectorAll(".category-btn");
+const searchButton = document.querySelector(".search-button");
 
-const url = "https://cms-ca-wp.dev-squid.com/wp-json/wc/store/products";
+const baseUrl = "https://cms-ca-wp.dev-squid.com/wp-json/wc/store/products";
 
-export async function getJackets() {
+let products = [];
+let filteredProducts = products;
+
+export async function getProducts(url) {
   try {
     const response = await fetch(url);
-    const products = await response.json();
+    products = await response.json();
 
     console.log(products);
+    displayProducts(products);
 
-    productsContainer.innerHTML = "";
-
-    products.forEach(function (product) {
-      productsContainer.innerHTML += `<a <article class="single-product product f-dir-col__center-center"   data-id="${product.id}" href="product.html?id=${product.id}&name=${product.name}" > 
-                  <img src="${product.images[0].src}" class="product-img img" alt=""/>
-                  <div class="product-footer">
-                    <h5 class="product-name h5-light">${product.name}</h5>
-                    <p class="product-price">${product.price_html}</p>
-                  </div>
-                </article></a>`;
-    });
+    // productsContainer.innerHTML = "";
   } catch (error) {
     console.log(error);
     productsContainer.innerHTML = message("error", "An error occurred.", error);
   }
 }
+getProducts(baseUrl);
 
-getJackets(url);
+const displayProducts = (productsToDisplay) => {
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<div class="msg-container">
+                                  <p class="error-msg" id="errorMsg">Sorry, no products matched your search</p>
+                                  </div>`;
+    return;
+  }
 
-//////////////////////////////////////////////////////////////////////////////
-////////////////// SETTING UP THE SEARCH INPUT FILTER/////////////////////////
-//////////////////////////////////////////////////////////////////////////////
+  productsContainer.innerHTML = filteredProducts
+    .map((product) => {
+      const { id, name, image, price } = product;
 
-// This is a search input text filter to show the products according to what we type in the search input
+      return `<a <div class="single-product product f-dir-col__center-center"   data-id="${product.id}" href="product.html?id=${product.id}&name=${product.name}" >
+                  <img src="${product.images[0].src}" class="product-img img" alt=""/>
+                  <div class="product-footer">
+                    <h5 class="product-name h5-light">${product.name}</h5>
+                    <p class="product-price">${product.price_html}</p>
+                  </div>
+                </div></a>`;
+    })
+    .join("");
+};
 
-// const form = document.querySelector(".input-form");
-// const searchInput = document.querySelector(".search-input");
-
-// form.addEventListener("keyup", () => {
-//   const inputValue = searchInput.value;
-//   console.log(inputValue);
-
-//   filteredProducts = products.filter((product) => {
-//     return product.name.toLowerCase().includes(inputValue);
+//   productsToDisplay.forEach(function (product) {
+//     productsContainer.innerHTML += `<a <div class="single-product product f-dir-col__center-center"   data-id="${product.id}" href="product.html?id=${product.id}&name=${product.name}" >
+//                   <img src="${product.images[0].src}" class="product-img img" alt=""/>
+//                   <div class="product-footer">
+//                     <h5 class="product-name h5-light">${product.name}</h5>
+//                     <p class="product-price">${product.price_html}</p>
+//                   </div>
+//                 </div></a>`;
 //   });
-//   displayProducts();
-// });
+// };
+displayProducts();
 
-//////////////////////////////////////////////////////////////////////////////
-////////// DISPLAYING THE FILTER BUTTONS FORM ( CATEGORY BUTTONS ) ///////////
-//////////////////////////////////////////////////////////////////////////////
+const form = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
 
-//
+form.addEventListener("keyup", () => {
+  const inputValue = searchInput.value;
+  console.log(inputValue);
 
-//////////////////////////////////////////////////////////////////////////////
-///// MAKING THE FILTER BUTTONS DISPLAY THE DATA ONCE THEY ARE CLICKED ///////
-//////////////////////////////////////////////////////////////////////////////
+  filteredProducts = products.filter((product) => {
+    return product.name.toLowerCase().includes(inputValue);
+  });
+  displayProducts();
+});
 
-// categoriesDOM.addEventListener("click", (e) => {
-//   // console.log(e.target);
-//   const htmlElement = e.target;
-//   if (htmlElement.classList.contains("category-btn")) {
-//     if (htmlElement.dataset.id === "all") {
-//       filteredProducts = [...products];
-//     } else {
-//       filteredProducts = products.filter((product) => {
-//         return product.category === htmlElement.dataset.id;
-//       });
-//     }
-//     searchInput.value = "";
-//     displayProducts();
+// filter code
+// do filtering method then call
+
+// let products = [];
+
+// export async function getProducts(url) {
+//   try {
+//     const response = await fetch(url);
+//     products = await response.json();
+//     console.log(products);
+
+//     // productsContainer.innerHTML = "";
+
+//     products.forEach(function (product) {
+//       productsContainer.innerHTML += `<a <div class="single-product product f-dir-col__center-center"   data-id="${product.id}" href="product.html?id=${product.id}&name=${product.name}" >
+//                   <img src="${product.images[0].src}" class="product-img img" alt=""/>
+//                   <div class="product-footer">
+//                     <h5 class="product-name h5-light">${product.name}</h5>
+//                     <p class="product-price">${product.price_html}</p>
+//                   </div>
+//                 </div></a>`;
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     productsContainer.innerHTML = message("error", "An error occurred.", error);
 //   }
+// }
+
+// getProducts(baseUrl);
+
+// // Items per-page filter
+// perPage.onchange = function (event) {
+//   const newUrl = baseUrl + `?per_page=${event.target.value}`;
+//   productsContainer.innerHTML = "";
+//   getProducts(newUrl);
+// };
+
+// // Categories Filter
+// categories.forEach(function (category) {
+//   category.onclick = function (event) {
+//     let newUrl;
+//     if (event.target.id === "all") {
+//       const categoryAll = event.target.value;
+//       newUrl = baseUrl + `?all=${categoryAll}`;
+//     } else {
+//       const categoryChosen = event.target.value;
+//       newUrl = baseUrl + `?category=${categoryChosen}`;
+//     }
+//     productsContainer.innerHTML = "";
+//     getProducts(newUrl);
+//   };
 // });
+
+// // //Search Input Filter
+// searchButton.onclick = function () {
+//   const searchInput = document.querySelector("#search-input").value;
+//   console.dir(searchInput);
+//   const newUrl = baseUrl + `?search=${searchInput}`;
+//   productsContainer.innerHTML = "";
+//   getProducts(newUrl);
+// };
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 import { navToggle } from "./components/nav.js";
-// import { getJackets } from "./js-pages/product-page.js";
+// import { getProducts } from "./js-pages/product-page.js";
+// import { perPage } from "./js-pages/product-page.js";
 // import { products } from "./js-pages/products.js";
